68425c846b6fbb60e5d68bab82b6075d330abb9e
ScottPurcival <admin@purcival.com>
2018-08-12 19:15:52 +1000

    fix to controller - updated music to singleton

---
+++ LD42/Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour {
+
+    static DontDestroy instance;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (!instance)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+            Destroy(this.gameObject);
+	}
+	
+}
