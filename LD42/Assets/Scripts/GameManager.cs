cf8836cbc859a3eb280f6ec29b5edc43bcecec09
ScottPurcival <admin@purcival.com>
2018-08-12 11:37:02 +1000

    mmade level loadr, addedd backgrouns, finished main menu

--- LD42/Assets/Scripts/GameManager.cs
+++ LD42/Assets/Scripts/GameManager.cs
@@ -83,6 +83,7 @@ public class GameManager : MonoBehaviour {
 
     void EndGame()
     {
+        NextScene.GoNextScene();
         Debug.Log("You Win!");
     }
 }
---
+++ LD42/Assets/Scripts/NextScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class NextScene {
+
+
+    public static void GoNextScene()
+    {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
